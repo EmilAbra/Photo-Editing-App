8347da81faab835b70baa7a597f8d869074cc3bd
eabra <emab21@student.bth.se>
2024-02-15 09:02:39 +0100

    Finished App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Slider from './Slider';
 import SidebarItem from './SidebarItem';
